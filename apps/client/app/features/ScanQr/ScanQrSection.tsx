d0e7a251dccfdc853a870fc5ab8a9ac4f3cbb602
ken-ttnka <ken.ttnka18@gmail.com>
2024-05-04 15:49:13 +0900

    edit app file

---
+++ apps/client/app/features/ScanQr/ScanQrSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LuScanLine } from "react-icons/lu";
+
+export const ScanQrSection = () => {
+    return (
+        <div className="justify-center">
+        <LuScanLine />
+        scan to transfer
+      </div>
+    );
+}
---
+++ apps/client/app/features/walletConnect/walletConnectSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const WalletConnectSection = () => {
+    return (
+        <div className="justify-center">
+            <button>wallet connect</button>
+        </div>
+    );
+};
