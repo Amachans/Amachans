e85c35f6d349ce4fa3307e0ba2d20d345b31d20b
Soule50431 <s.nove.trece.50431@gmail.com>
2024-05-12 11:00:13 +0000

    fix to be able to build client

---
+++ apps/client/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+// /dashboard/page.tsx
+"use client";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Dashboard = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <div>
+        <div>My Wallet</div>
+        <div>TODO: getAddressした結果をQRに変換して表示する</div>
+      </div>
+
+      <div>Address</div>
+
+      <div>Balance</div>
+
+      <div>
+        <Link href="transfer">
+          {" "}
+          {/* Linkコンポーネントでラップ */}
+          <Button>Exit</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
